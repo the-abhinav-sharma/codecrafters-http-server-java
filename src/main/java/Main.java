13ed9890d1e3cbb5c87282ea86eaf502d5d7cb59
the-abhinav-sharma <theabhinavsharma@hotmail.com>
2024-09-23 18:57:50 +0530

    Respond with 200

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,7 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -16,8 +19,14 @@ public class Main {
        // ensures that we don't run into 'Address already in use' errors
        serverSocket.setReuseAddress(true);
     
-       serverSocket.accept(); // Wait for connection from client.
+       Socket socket = serverSocket.accept(); // Wait for connection from client.
        System.out.println("accepted new connection");
+       
+       PrintWriter out = new PrintWriter(socket.getOutputStream(),true);
+       BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+       out.println("HTTP/1.1 200 OK\r\n\r\n");
+       out.close();
+       socket.close();
      } catch (IOException e) {
        System.out.println("IOException: " + e.getMessage());
      }
